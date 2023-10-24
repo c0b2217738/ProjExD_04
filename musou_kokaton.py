0a75006476354f9abb74bda1e7ea44487ee44015
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2023-10-24 16:17:18 +0900

    追加機能1実装ほぼ完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -90,11 +90,6 @@ class Bird(pg.sprite.Sprite):
         sum_mv = [0, 0]
         for k, mv in __class__.delta.items():
             if key_lst[k]:
-                for event in pg.event.get():
-                    if event.type == pg.KEYDOWN and event.key == pg.K_LSHIFT:
-                        self.speed = 20
-                    else:
-                        self.speed = 10
                 self.rect.move_ip(+self.speed*mv[0], +self.speed*mv[1])
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
@@ -275,6 +270,10 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            if event.type == pg.KEYDOWN and event.key == pg.K_LSHIFT:
+                bird.speed = 20
+            else:
+                bird.speed = 10
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
